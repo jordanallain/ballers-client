ba91126e627262a0809b0fba26efcc1cfd022964
jordanallain <jordan.allain@generalassemb.ly>
2018-07-28 14:13:41 -0400

    Add event listener to get players button
    - Add event listener to get players button.
    - Use fetch to make request to back end local server.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-document.onreadystatechange = function () {
-    if (document.readyState === "interactive") {
-        console.log('hello')
+const store = {}
+
+const onGetPlayers = function () {
+  fetch('http://localhost:4741', {
+    method: 'GET',
+    mode: 'no-cors',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
     }
+  })
+    .then(console.log)
+    .catch(console.error)
+}
+
+document.onreadystatechange = function () {
+  if (document.readyState === "interactive") {
+    const getPlayers = document.getElementById('get-data')
+    getPlayers.addEventListener('click', onGetPlayers)
+  }
 }
